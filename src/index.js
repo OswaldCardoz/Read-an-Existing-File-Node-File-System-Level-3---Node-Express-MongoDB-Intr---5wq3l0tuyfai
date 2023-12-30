f0860ecd355863727b831100f2546ca9984db5e6
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-30 21:29:40 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
 	// dont change function name
+  try {
+    // Use fs.promises.readFile to read the content of the file
+    const content = await fs.promises.readFile(filename, "utf-8");
+
+    // Return the content of the file
+    return content;
+  } catch (error) {
+    // Handle any errors that might occur during file reading
+    console.error("Error reading the file:", error.message);
+    throw error;
+  }
   
 };
 
